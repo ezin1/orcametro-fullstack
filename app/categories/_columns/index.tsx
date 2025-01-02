d517c9b750d7207fc23f68a15c3f8a728d0c6a3a
ezin1 <eziof.dev@gmail.com>
2025-01-02 16:13:24 -0300

    feat: create initial configs to dropdown list actions

--- app/categories/_columns/index.tsx
+++ app/categories/_columns/index.tsx
@@ -1,36 +1,34 @@
-"use client";
-import { Categories } from "@prisma/client";
-import { ColumnDef } from "@tanstack/react-table";
-import CategoryTypeBadge from "../_components/category-type-badge";
-import { DrawerUpsertCategory } from "../_components/drawer-upsert-category";
-import DeleteCategoryButton from "../_components/delete-category-button";
+// "use client";
+// import { Categories } from "@prisma/client";
+// import { ColumnDef } from "@tanstack/react-table";
+// import CategoryTypeBadge from "../_components/category-type-badge";
+// import DropdownListActions from "../_components/dropdown-list-actions";
 
-export const categoriesColumns: ColumnDef<Categories>[] = [
-  {
-    accessorKey: "categoryType",
-    header: "Tipo de categoria",
-    cell: ({ row: { original: category } }) => (
-      <CategoryTypeBadge category={category} />
-    ),
-  },
-  {
-    accessorKey: "code",
-    header: "Código",
-  },
-  {
-    accessorKey: "name",
-    header: "Nome",
-  },
-  {
-    accessorKey: "actions",
-    header: "Ações",
-    cell: ({ row: { original: category } }) => {
-      return (
-        <div className="flex flex-row space-x-1">
-          <DrawerUpsertCategory category={category} isUpdate={true} />
-          <DeleteCategoryButton id={category.id} />
-        </div>
-      );
-    },
-  },
-];
+// export const categoriesColumns: ColumnDef<Categories>[] = [
+//   {
+//     accessorKey: "categoryType",
+//     header: "Tipo de categoria",
+//     cell: ({ row: { original: category } }) => (
+//       <CategoryTypeBadge category={category} />
+//     ),
+//   },
+//   {
+//     accessorKey: "code",
+//     header: "Código",
+//   },
+//   {
+//     accessorKey: "name",
+//     header: "Nome",
+//   },
+//   {
+//     accessorKey: "actions",
+//     header: "Ações",
+//     cell: ({ row: { original: category } }) => {
+//       return (
+//         <div className="flex flex-row space-x-1">
+//           <DropdownListActions category={category}/>
+//         </div>
+//       );
+//     },
+//   },
+// ];
---
+++ app/categories/_components/dropdown-list-actions.tsx
@@ -0,0 +1,35 @@
+// import { Button } from "@/app/_components/ui/button"
+// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/app/_components/ui/dropdown-menu"
+// import { CategoryType } from "@prisma/client";
+// import { MoreHorizontal } from "lucide-react"
+// import { DrawerUpsertCategory } from "./drawer-upsert-category";
+
+// interface DropdownListActionsProps {
+//     category: {
+//         name: string;
+//         id: string;
+//         userId: string;
+//         categoryType: CategoryType;
+//         code: string;
+//     }
+// }
+// const DropdownListActions = ({category}: DropdownListActionsProps) => {
+//     return (
+//         <DropdownMenu>
+//           <DropdownMenuTrigger asChild>
+//             <Button variant="ghost" className="h-8 w-8 p-0">
+//               <span className="sr-only">Abrir menu</span>
+//               <MoreHorizontal />
+//             </Button>
+//           </DropdownMenuTrigger>
+//           <DropdownMenuContent align="end">
+//             <DropdownMenuLabel>Ações</DropdownMenuLabel>
+//             <DropdownMenuSeparator />
+//             <DropdownMenuItem>Editar</DropdownMenuItem>
+//             <DropdownMenuItem>View payment details</DropdownMenuItem>
+//           </DropdownMenuContent>
+//         </DropdownMenu>
+//       )
+// }
+
+// export default DropdownListActions
