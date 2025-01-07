622290bef788c2f73c2e2c88e7b4685f7b394b28
ezin1 <eziof.dev@gmail.com>
2025-01-07 07:58:39 -0300

    feat: create initial subs page

---
+++ app/subscriptions/_components/card-plan.tsx
@@ -0,0 +1,82 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { CheckIcon } from "lucide-react";
+
+interface CardPlanProps {
+  plans: {
+    name: string;
+    description: string;
+    price: number;
+    features: string[];
+  }[];
+  userPlan: string;
+}
+
+const CardPlan = ({ plans, userPlan }: CardPlanProps) => {
+  return (
+    <ScrollArea className="h-[700px]">
+      <div className="flex w-full flex-col items-center justify-between gap-6 sm:flex-row md:flex-row lg:flex-row xl:flex-row">
+        {plans.map((plans) => (
+          <Card key={plans.name} className="w-[300px]">
+            <CardHeader className="relative items-center justify-center">
+              {userPlan === plans.name ? (
+                <Badge className="absolute right-4 top-4 bg-primary text-white">
+                  Seu plano
+                </Badge>
+              ) : null}
+              <CardTitle className="text-3xl font-bold">{plans.name}</CardTitle>
+              <CardDescription>{plans.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex items-center justify-center pb-5">
+                <div className="flex flex-col pr-2">
+                  <span className="text-xl">POR</span>
+                  <span className="text-4xl">R$</span>
+                </div>
+                <span className="mb-1 pr-1 text-7xl font-bold">
+                  {Math.floor(plans.price)}
+                </span>
+                <div className="flex flex-col">
+                  <span className="text-4xl font-bold">
+                    ,{plans.price.toString().split(".")[1]}
+                  </span>
+                  <div className="text-2xl text-muted-foreground">/mês</div>
+                </div>
+              </div>
+              <ul className="border-t pt-3">
+                {plans.features.map((feature) => (
+                  <li
+                    className="flex flex-row items-center justify-start gap-2"
+                    key={feature}
+                  >
+                    {feature}
+                    <CheckIcon className="text-primary" />
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+            <CardFooter className="flex w-full items-center p-4">
+              <Button
+                variant={userPlan === plans.name ? "outline" : "default"}
+                className="w-full rounded-full font-bold text-white"
+              >
+                {userPlan === plans.name ? "Gerenciar plano" : "Contratar"}
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </ScrollArea>
+  );
+};
+
+export default CardPlan;
--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { usersInfo } from "../_data/users/users-info";
+import CardPlan from "./_components/card-plan";
 
 const SubscripionsPage = async () => {
   const { userId } = await auth();
@@ -15,9 +16,39 @@ const SubscripionsPage = async () => {
     redirect("/register");
   }
 
+  const userPlan = userInfo.verifyIfUserIsRegistered.userPlan;
+
+  const arrayPlans = [
+    {
+      name: "Basic",
+      description: "Plano inicial",
+      price: 15.99,
+      features: ["Feature 1", "Feature 2", "Feature 3"],
+    },
+    {
+      name: "Standard",
+      description: "Plano intermediário",
+      price: 23.99,
+      features: ["Feature 1", "Feature 2", "Feature 3"],
+    },
+    {
+      name: "Premium",
+      description: "Plano completo",
+      price: 35.99,
+      features: ["Feature 1", "Feature 2", "Feature 3"],
+    },
+  ];
+
   return (
-    <div>
-      <p>Subscripions Page</p>
+    <div className="flex h-screen flex-col space-y-6 overflow-hidden p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-base font-bold sm:text-sm md:text-lg lg:text-2xl">
+          Planos
+        </h1>
+      </div>
+      <div>
+        <CardPlan plans={arrayPlans} userPlan={userPlan} />
+      </div>
     </div>
   );
 };
