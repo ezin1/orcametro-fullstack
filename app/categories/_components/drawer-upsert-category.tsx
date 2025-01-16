25f0e4123248a891985e1bba6854117ce94e9ffa
ezin1 <eziof.dev@gmail.com>
2025-01-16 10:33:26 -0300

    feat: convert drawer upsert seller label to use select and input label in border

--- app/categories/_components/drawer-upsert-category.tsx
+++ app/categories/_components/drawer-upsert-category.tsx
@@ -1,4 +1,3 @@
-import { AlertDestructive } from "@/app/_components/alert-dialog";
 import { Button } from "@/app/_components/ui/button";
 import {
   Drawer,
@@ -15,22 +14,20 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
 } from "@/app/_components/ui/form";
-import { Input } from "@/app/_components/ui/input";
+import { InputLabelInBorder } from "@/app/_components/ui/input-label-in-border";
 import {
-  Select,
   SelectContent,
   SelectItem,
-  SelectTrigger,
   SelectValue,
 } from "@/app/_components/ui/select";
+import { SelectLabelInBorder } from "@/app/_components/ui/select-label-in-border";
 import { CATEGORIES_TYPES_OPTIONS } from "@/app/_constants/categories";
 import { categoryUpsert } from "@/app/_data/categories/category-upsert";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CategoryType } from "@prisma/client";
 import { ClipboardPlus, Loader2Icon, PencilIcon } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -47,7 +44,7 @@ interface UpsertCategoryProps {
 const formSchema = z.object({
   code: z
     .string()
-    .min(3, { message: "O código deve ter no mínimo 3 caracteres" }),
+    .min(1, { message: "O código deve ter no mínimo 1 caracteres" }),
   name: z
     .string()
     .min(3, { message: "O nome deve ter no mínimo 3 caracteres" }),
@@ -65,7 +62,6 @@ export function DrawerUpsertCategory({
   isUpdate,
 }: UpsertCategoryProps) {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
   const [categoryRegisterIsLoading, setCategoryRegisterIsLoading] =
     useState(false);
   const form = useForm<FormSchemaCategoryUpsert>({
@@ -92,14 +88,6 @@ export function DrawerUpsertCategory({
     }
   };
 
-  useEffect(() => {
-    if (Object.keys(form.formState.errors).length > 0) {
-      setShowAlert(true);
-    } else {
-      setShowAlert(false);
-    }
-  }, [form.formState.errors]);
-
   return (
     <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
       <DrawerTrigger>
@@ -125,18 +113,20 @@ export function DrawerUpsertCategory({
                     : "Preencha os campos abaixo para cadastrar uma nova categoria"}
                 </DrawerDescription>
               </DrawerHeader>
-              {showAlert && <AlertDestructive />}
-              <div className="space-y-2 p-4 pb-0">
+              <div className="space-y-5 p-4 pb-0">
                 <FormField
                   control={form.control}
                   name="code"
                   render={({ field }) => (
-                    <FormItem className="flex flex-row items-center gap-3">
-                      <FormLabel className="text-sm font-bold">
-                        Código
-                      </FormLabel>
+                    <FormItem>
                       <FormControl>
-                        <Input placeholder="Digite o código..." {...field} />
+                        <InputLabelInBorder
+                          label="Código"
+                          placeholder="Digite o código..."
+                          {...field}
+                          error={!!form.formState.errors.code}
+                          errorMessage={form.formState.errors.code?.message}
+                        />
                       </FormControl>
                     </FormItem>
                   )}
@@ -145,10 +135,15 @@ export function DrawerUpsertCategory({
                   control={form.control}
                   name="name"
                   render={({ field }) => (
-                    <FormItem className="flex flex-row items-center gap-3">
-                      <FormLabel className="text-sm font-bold">Nome</FormLabel>
+                    <FormItem>
                       <FormControl>
-                        <Input placeholder="Digite o nome..." {...field} />
+                        <InputLabelInBorder
+                          label="Nome"
+                          placeholder="Digite o nome..."
+                          {...field}
+                          error={!!form.formState.errors.name}
+                          errorMessage={form.formState.errors.name?.message}
+                        />
                       </FormControl>
                     </FormItem>
                   )}
@@ -158,26 +153,29 @@ export function DrawerUpsertCategory({
                   name="categoryType"
                   render={({ field }) => (
                     <FormItem className="flex flex-row items-center gap-3">
-                      <FormLabel className="text-sm font-bold">
-                        Tipo de categoria
-                      </FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Selecione um tipo de categoria" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {CATEGORIES_TYPES_OPTIONS.map((option) => (
-                            <SelectItem key={option.value} value={option.value}>
-                              {option.label}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
+                      <FormControl>
+                        <SelectLabelInBorder
+                          label="Tipo de categoria"
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          error={!!form.formState.errors.categoryType}
+                          errorMessage={
+                            form.formState.errors.categoryType?.message
+                          }
+                        >
+                          <SelectValue placeholder="Selecione um tipo de categoria" />
+                          <SelectContent>
+                            {CATEGORIES_TYPES_OPTIONS.map((option) => (
+                              <SelectItem
+                                key={option.value}
+                                value={option.value}
+                              >
+                                {option.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </SelectLabelInBorder>
+                      </FormControl>
                     </FormItem>
                   )}
                 />
@@ -188,7 +186,7 @@ export function DrawerUpsertCategory({
                 </DrawerClose>
                 <Button className="text-white" type="submit">
                   {categoryRegisterIsLoading && (
-                    <Loader2Icon className="animate-spin" />
+                    <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
                   )}
                   Salvar
                 </Button>
