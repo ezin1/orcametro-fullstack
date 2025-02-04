ec096af4fd734c6458a7c5abaf2f445580e41f95
ezin1 <eziof.dev@gmail.com>
2025-02-04 16:16:28 -0300

    feat: add test tooltip to approved budget status badge for additional context

--- app/budgets/myBudgets/_components/badge-budget-status.tsx
+++ app/budgets/myBudgets/_components/badge-budget-status.tsx
@@ -1,17 +1,35 @@
 import { Badge } from "@/app/_components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/app/_components/ui/tooltip";
 import { BudgetStatus } from "@prisma/client";
 import { CircleCheck, CircleX, Clock } from "lucide-react";
 
 interface BadgeBudgetStatusProps {
   budgetStatus: BudgetStatus;
+  createdAt: Date;
+  expirationDate: Date;
+  updatedAt: Date;
 }
 const BadgeBudgetStatus = ({ budgetStatus }: BadgeBudgetStatusProps) => {
   if (budgetStatus === BudgetStatus.APPROVED) {
     return (
-      <Badge className="border-[hsl(160,60%,45%)] bg-transparent font-bold text-[hsl(160,60%,45%)] hover:bg-accent">
-        <CircleCheck size={13} className="mr-2" />
-        Aprovado
-      </Badge>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Badge className="border-[hsl(160,60%,45%)] bg-transparent font-bold text-[hsl(160,60%,45%)] hover:bg-accent">
+              <CircleCheck size={13} className="mr-2" />
+              Aprovado
+            </Badge>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     );
   }
   if (budgetStatus === BudgetStatus.PENDING) {
